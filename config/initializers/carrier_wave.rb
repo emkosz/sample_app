3875979eb1684a54ba599d23371a1b88396ede9f
Emma Koszinowski <emkosz@gmail.com>
2016-01-19 11:53:11 -0800

    Add S3 acces keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
-	CarrierWave.configure do |config|
-		config.fog_credentials = {
-			# Configuration for Amazon S3
-			:provider				=> 'AWS',
-			:aws_access_key_id		=> ENV['AKIAIEKQ7YFBP73RB2KQ'],
-			:aws_secret_access_key	=> ENV['W+uB6Zn5hnbPx1bzUc1eGhBK8KPOC4BzJKYkp1qc']
-		}
-		config.fog_directory		= ENV['emkosz-sampleapp']
-	end
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['AKIAIEKQ7YFBP73RB2KQ'],
+      :aws_secret_access_key => ENV['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']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
