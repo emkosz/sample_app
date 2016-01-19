537f086ca31cf0d8fc4a275a1ef102c89165c9c2
Emma Koszinowski <emkosz@gmail.com>
2016-01-19 12:10:53 -0800

    Changed S3 bucket name

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,6 +6,6 @@ if Rails.env.production?
       :aws_access_key_id     => ENV['AKIAIEKQ7YFBP73RB2KQ'],
       :aws_secret_access_key => ENV['W+uB6Zn5hnbPx1bzUc1eGhBK8KPOC4BzJKYkp1qc']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  ENV['emkosz-sampleapp']
   end
 end
