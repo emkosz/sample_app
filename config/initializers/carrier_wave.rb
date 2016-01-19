eeb47f9e3064869f1291566554a552a161354805
Emma Koszinowski <emkosz@gmail.com>
2016-01-19 12:42:44 -0800

    Add S3 region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,9 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIEKQ7YFBP73RB2KQ'],
-      :aws_secret_access_key => ENV['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']
+      :provider              	=> 'AWS',
+      :aws_access_key_id     	=> ENV['AKIAIEKQ7YFBP73RB2KQ'],
+      :aws_secret_access_key 	=> ENV['W+uB6Zn5hnbPx1bzUc1eGhBK8KPOC4BzJKYkp1qc'],
+      :region									=> ENV['us-west-1']
     }
     config.fog_directory     =  ENV['emkosz-sampleapp']
   end
