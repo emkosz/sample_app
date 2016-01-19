49f6b19f39aa5b7a540c3ed5e6ad32ca1edbf083
Emma Koszinowski <emkosz@gmail.com>
2016-01-19 12:56:50 -0800

    Corrected S3 info

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,11 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider              	=> 'AWS',
-      :aws_access_key_id     	=> ENV['AKIAIEKQ7YFBP73RB2KQ'],
-      :aws_secret_access_key 	=> ENV['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'],
-      :region									=> ENV['us-west-1']
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['emkosz-sampleapp']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
